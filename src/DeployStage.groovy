3119d7c2648fa5f70450e9659545dd09063a2551
Keith Manning <kmanning@users.noreply.github.com>
2021-05-05 16:21:51 -0400

    Issue 21: Create a Jenkinsfile stage per environment

--- src/DeployStage.groovy
+++ src/DeployStage.groovy
@@ -1,10 +1,16 @@
 public class DeployStage implements Stage {
+    private String environment
+
     public DeployStage(String environment) {
-        println "DeployStage(${environment})"
+        this.environment = environment
     }
 
     @Override
     public Closure pipelineConfiguration() {
-        return { -> }
+        return {
+            stage("deploy-${environment}") {
+                println "do the thing"
+            }
+        }
     }
 }
--- test/DeployStageTest.groovy
+++ test/DeployStageTest.groovy
@@ -1,5 +1,9 @@
 import static org.hamcrest.Matchers.instanceOf
 import static org.hamcrest.MatcherAssert.assertThat
+import static org.mockito.Mockito.any
+import static org.mockito.Mockito.eq
+import static org.mockito.Mockito.spy
+import static org.mockito.Mockito.verify
 
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -21,6 +25,18 @@ class DeployStageTest {
 
             def result = deployStage.pipelineConfiguration()
             assertThat(result, instanceOf(Closure.class))
+        }
+
+        @Test
+        void createsAStageNamedAfterTheEnvironment() {
+            def deployStage = new DeployStage('qa')
+            def workflowScript = spy(new MockWorkflowScript())
+
+            def pipelineConfiguration = deployStage.pipelineConfiguration()
+            pipelineConfiguration.delegate = workflowScript
+            pipelineConfiguration()
+
+            verify(workflowScript).stage(eq("deploy-qa"), any(Closure.class))
         }
     }
 }
