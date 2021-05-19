7b879ee9a8675322d18405cd31ea0bdf6224902a
Keith Manning <kmanning@users.noreply.github.com>
2021-05-19 13:03:53 -0400

    Issue 7: Decorate the BuildStage

--- src/StashUnstashPlugin.groovy
+++ src/StashUnstashPlugin.groovy
@@ -1,5 +1,17 @@
-public class StashUnstashPlugin {
+public class StashUnstashPlugin implements Plugin {
     public static withArtifact(String artifact) {
         return this
+    }
+
+    public static void init() {
+        StagePlugins.add(new StashUnstashPlugin(), BuildStage.class)
+    }
+
+    public void apply(Stage stage) {
+        stage.decorate(stashDecoration())
+    }
+
+    public Closure stashDecoration() {
+        return { }
     }
 }
--- test/StashUnstashPluginTest.groovy
+++ test/StashUnstashPluginTest.groovy
@@ -1,10 +1,24 @@
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.hasItem
+import static org.hamcrest.Matchers.instanceOf
+import static org.mockito.Mockito.doReturn
+import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.spy
+import static org.mockito.Mockito.verify
 
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class StashUnstashPluginTest {
+    @BeforeEach
+    @AfterEach
+    public void reset() {
+        StagePlugins.reset()
+    }
+
     @Nested
     public class WithArtifact {
         @Test
@@ -12,6 +26,32 @@ class StashUnstashPluginTest {
             def result = StashUnstashPlugin.withArtifact('myArtifact.jar')
 
             assertThat(result, equalTo(StashUnstashPlugin.class))
+        }
+    }
+
+    @Nested
+    public class Init {
+        @Test
+        void addsStashUnstashPluginToBuildStage() {
+            StashUnstashPlugin.init()
+            def results = StagePlugins.getPluginsFor(new BuildStage())
+
+            assertThat(results, hasItem(instanceOf(StashUnstashPlugin.class)))
+        }
+    }
+
+    @Nested
+    public class Apply {
+        @Test
+        void decoratesTheBuildStage() {
+            def buildStage = mock(BuildStage.class)
+            def stashDecoration = { }
+            def plugin = spy(new StashUnstashPlugin())
+            doReturn(stashDecoration).when(plugin).stashDecoration()
+
+            plugin.apply(buildStage)
+
+            verify(buildStage).decorate(stashDecoration)
         }
     }
 }
