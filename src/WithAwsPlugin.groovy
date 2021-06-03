f317dc99cb2edd3b9f1d43d1f817e3201a6f223b
Keith Manning <kmanning@users.noreply.github.com>
2021-06-03 09:32:35 -0400

    Issue 46: sketch out the init method

--- src/WithAwsPlugin.groovy
+++ src/WithAwsPlugin.groovy
@@ -1,5 +1,13 @@
-class WithAwsPlugin {
+class WithAwsPlugin implements Plugin {
+    public static init() {
+        StagePlugins.add(new WithAwsPlugin(), DeployStage)
+    }
+
     public static withRole() {
         return this
+    }
+
+    public void apply(Stage stage) {
+        println "Do the thing"
     }
 }
--- test/WithAwsPluginTest.groovy
+++ test/WithAwsPluginTest.groovy
@@ -1,5 +1,8 @@
 import static org.hamcrest.MatcherAssert.assertThat
 import static org.hamcrest.Matchers.equalTo
+import static org.hamcrest.Matchers.hasItem
+import static org.hamcrest.Matchers.instanceOf
+import static org.mockito.Mockito.mock
 
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -20,6 +23,18 @@ class WithAwsPluginTest {
             def result = WithAwsPlugin.withRole()
 
             assertThat(result, equalTo(WithAwsPlugin.class))
+        }
+    }
+
+    @Nested
+    public class Init {
+        @Test
+        void addsPluginToTheDeployStage() {
+            WithAwsPlugin.init()
+
+            def plugins = StagePlugins.getPluginsFor(mock(DeployStage))
+
+            assertThat(plugins, hasItem(instanceOf(WithAwsPlugin)))
         }
     }
 }
