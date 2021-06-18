2fb06ecad9f5d44d15dbf1930a1189def67722cf
Keith Manning <kmanning@users.noreply.github.com>
2021-06-18 15:06:33 -0400

    Issue 59: capture the original Jenkinsfile workflowscript

--- src/Jenkinsfile.groovy
+++ src/Jenkinsfile.groovy
@@ -1,1 +1,11 @@
+public class Jenkinsfile {
+    private static original
 
+    public static void init(original) {
+        this.original = original
+    }
+
+    public static getInstance() {
+        return original
+    }
+}
