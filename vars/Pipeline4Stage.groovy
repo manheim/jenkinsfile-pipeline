a8cc7f04ffa319b98bff505f1ea3575216010ab2
Keith Manning <kmanning@users.noreply.github.com>
2021-06-24 17:27:17 -0400

    Issue 61: add template for 4 stage pipeline

---
+++ vars/Pipeline4Stage.groovy
@@ -0,0 +1,48 @@
+def call(args) {
+    pipeline {
+        agent none
+        options { preserveStashes() }
+
+        stages {
+            stage('1') {
+                steps {
+                    script {
+                        def configuration = ((Stage)args.getAt(0)).pipelineConfiguration()
+                        configuration.delegate = this
+                        configuration()
+                    }
+                }
+            }
+
+            stage('2') {
+                steps {
+                    script {
+                        def configuration = ((Stage)args.getAt(1)).pipelineConfiguration()
+                        configuration.delegate = this
+                        configuration()
+                    }
+                }
+            }
+
+            stage('3') {
+                steps {
+                    script {
+                        def configuration = ((Stage)args.getAt(2)).pipelineConfiguration()
+                        configuration.delegate = this
+                        configuration()
+                    }
+                }
+            }
+
+            stage('4') {
+                steps {
+                    script {
+                        def configuration = ((Stage)args.getAt(3)).pipelineConfiguration()
+                        configuration.delegate = this
+                        configuration()
+                    }
+                }
+            }
+        }
+    }
+}
