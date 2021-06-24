e9d778dc541b9807b5d764b1428dab1aef368f4e
Keith Manning <kmanning@users.noreply.github.com>
2021-06-24 17:08:19 -0400

    Issue 61: use the correct template depending on the number of stages

--- src/DeclarativePipeline.groovy
+++ src/DeclarativePipeline.groovy
@@ -1,6 +1,9 @@
 class DeclarativePipeline {
+    private workflowScript
+    private List<Stage> stages = []
+
     public DeclarativePipeline(workflowScript) {
-        println "do the thing"
+        this.workflowScript = workflowScript
     }
 
     public DeclarativePipeline withNodeLabel(String label) {
@@ -8,14 +11,41 @@ class DeclarativePipeline {
     }
 
     public DeclarativePipeline startsWith(Stage stage) {
+        stages = [stage]
         return this
     }
 
     public DeclarativePipeline then(Stage stage) {
+        stages << stage
         return this
     }
 
     public void build() {
-        println "build the pipeline"
+        def template = getPipelineTemplate(stages)
+
+        template.call(stages)
+    }
+
+    public getPipelineTemplate(List<Stage> stages) {
+        switch (stages.size()) {
+            case 0:
+                return workflowScript.Pipeline0Stage
+            case 1:
+                return workflowScript.Pipeline1Stage
+            case 2:
+                return workflowScript.Pipeline2Stage
+            case 3:
+                return workflowScript.Pipeline3Stage
+            case 4:
+                return workflowScript.Pipeline4Stage
+            case 5:
+                return workflowScript.Pipeline5Stage
+            case 6:
+                return workflowScript.Pipeline6Stage
+            case 7:
+                return workflowScript.Pipeline7Stage
+        }
+
+        return null
     }
 }
--- test/DeclarativePipelineTest.groovy
+++ test/DeclarativePipelineTest.groovy
@@ -1,6 +1,9 @@
 import static org.hamcrest.Matchers.equalTo
 import static org.hamcrest.MatcherAssert.assertThat
+import static org.mockito.Mockito.any
+import static org.mockito.Mockito.doReturn
 import static org.mockito.Mockito.mock
+import static org.mockito.Mockito.spy
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Nested
@@ -54,11 +57,148 @@ class DeclarativePipelineTest {
     @Nested
     public class Build {
         @Test
-        void doesNotFail() {
-            def pipeline = new DeclarativePipeline(new MockWorkflowScript())
+        void buildsThePipelineUsingTheAppropriateDeclarativeTemplate() {
+            def wasCalled = false
+            def pipelineTemplate = { wasCalled = true }
+            def pipeline = spy(new DeclarativePipeline(new MockWorkflowScript()))
+            doReturn(pipelineTemplate).when(pipeline).getPipelineTemplate(any(List))
 
             pipeline.build()
+
+            assertThat(wasCalled, equalTo(true))
         }
+
+        @Test
+        void correctlyOrdersTheStages() {
+            def expectedStages = [mock(Stage), mock(Stage), mock(Stage)]
+            def actualStages = null
+            def pipelineTemplate = { stages -> actualStages = stages }
+            def pipeline = spy(new DeclarativePipeline(new MockWorkflowScript()))
+            doReturn(pipelineTemplate).when(pipeline).getPipelineTemplate(any(List))
+
+            pipeline.startsWith(expectedStages[0])
+                    .then(expectedStages[1])
+                    .then(expectedStages[2])
+                    .build()
+
+            assertThat(actualStages, equalTo(expectedStages))
+        }
+    }
+
+    @Nested
+    public class GetPipelineTemplate {
+        @Test
+        void returnsEmptyDeclarativeTemplateWhenZeroStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline0Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def result = pipeline.getPipelineTemplate([])
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForOneStageWhenOneStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline1Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def result = pipeline.getPipelineTemplate([mock(Stage)])
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForTwoStagesWhenTwoStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline2Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def stages = (1..2).collect { mock(Stage) }
+            def result = pipeline.getPipelineTemplate(stages)
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForThreeStagesWhenThreeStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline3Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def stages = (1..3).collect { mock(Stage) }
+            def result = pipeline.getPipelineTemplate(stages)
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForFourStagesWhenFourStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline4Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def stages = (1..4).collect { mock(Stage) }
+            def result = pipeline.getPipelineTemplate(stages)
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForFiveStagesWhenFiveStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline5Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def stages = (1..5).collect { mock(Stage) }
+            def result = pipeline.getPipelineTemplate(stages)
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForSixStagesWhenSixStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline6Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def stages = (1..6).collect { mock(Stage) }
+            def result = pipeline.getPipelineTemplate(stages)
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+
+        @Test
+        void returnsDeclarativeTemplateForSevenStagesWhenSevenStagesAdded() {
+            def expectedTemplate = { }
+            def workflowScript = new PipelineTemplateMockWorkflowScript()
+            workflowScript.Pipeline7Stage = expectedTemplate
+            def pipeline = new DeclarativePipeline(workflowScript)
+
+            def stages = (1..7).collect { mock(Stage) }
+            def result = pipeline.getPipelineTemplate(stages)
+
+            assertThat(result, equalTo(expectedTemplate))
+        }
+    }
+
+    public class PipelineTemplateMockWorkflowScript extends MockWorkflowScript {
+        public Pipeline0Stage
+        public Pipeline1Stage
+        public Pipeline2Stage
+        public Pipeline3Stage
+        public Pipeline4Stage
+        public Pipeline5Stage
+        public Pipeline6Stage
+        public Pipeline7Stage
     }
 }
 
